5df6910e4475c6d7236a8eb406e9b5e8901edae6
abhaj2 <74650828+abhaj2@users.noreply.github.com>
2022-03-30 12:18:14 +0530

    Create s3.1.py

---
+++ s3.1.py
@@ -0,0 +1,9 @@
+import numpy as np
+m1 = np.array ([[1,5,2], [2,1,4], [2,2,1]])
+print("The first 2D matrix is \n ", m1)
+m2 = np.array([[2,5,1],[5,3,1],[1,1,0]])
+print("The second 2D matrix is \n ", m2)
+sum=np.add(m1,m2)
+print("The sum of two  2D matrix is \n ", sum )
+sub = np.subtract(m1,m2)
+print("The difference between two  2D matrix is \n ", sub )
